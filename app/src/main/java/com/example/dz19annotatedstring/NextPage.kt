d5c9a679e5ee4b05025d59d0921a57895baf4e12
OlegEfremov54 <ole_f@mail.ru>
2025-01-29 12:11:32 +0300

    Комит 1

--- app/src/main/java/com/example/dz19annotatedstring/NextPage.kt
+++ app/src/main/java/com/example/dz19annotatedstring/NextPage.kt
@@ -3,13 +3,9 @@ package com.example.dz19annotatedstring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.ParagraphStyle
@@ -19,8 +15,6 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dz19annotatedstring.ui.theme.DZ19AnnotatedStringTheme
 
